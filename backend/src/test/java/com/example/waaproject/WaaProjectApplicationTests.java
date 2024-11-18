b8be279f2e979ba21fb477aaf57096dbb4b9aee8
yohannnes <johnysg064@gmail.com>
2024-11-18 14:58:40 -0600

    intial backend setup

---
+++ backend/src/test/java/com/example/waaproject/WaaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
