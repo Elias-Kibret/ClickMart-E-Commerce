85da0c6dd3ae92436329f3a6a1c0bb026724eb8e
Yared Weldegebrial <yared12707@gmail.com>
2024-11-20 15:12:26 -0600

    backend updated

---
+++ backend/eCommersApp/src/main/java/Ecom/ModelDTO/ProductDTO.java
@@ -0,0 +1,52 @@
+package Ecom.ModelDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class ProductDTO {
+
+    @NotNull(message = "Product name is Mandatory ,can Not Be Null")
+    @NotBlank(message = "Product name is Mandatory")
+    private String name;
+
+    @NotNull(message = "Product Image is Mandatory ,can Not Be Null")
+    @NotBlank(message = "Product Image is Mandatory")
+    private String imageUrl;
+
+    @NotNull(message = "Product description is Mandatory ,can Not Be Null")
+    @NotBlank(message = "Product description is Mandatory")
+    @Size(min = 10, max = 200)
+    private String description;
+
+    @NotNull(message = "Product price is Mandatory ,can Not Be Null")
+    private Double price;
+
+    @NotNull(message = "Product category_name is Mandatory ,can Not Be Null")
+    @NotBlank(message = "Product category_name is Mandatory")
+    private String category;
+
+
+    @ElementCollection
+    @NotEmpty(message = "At least one image is required.")
+    private List<String> relatedImages;
+
+
+    @ElementCollection
+    @NotEmpty(message = "At least one color is required.")
+    private List<String> colors;
+
+
+    @ElementCollection
+    @NotEmpty(message = "At least one size is required.")
+    private List<String> sizes;
+
+}
